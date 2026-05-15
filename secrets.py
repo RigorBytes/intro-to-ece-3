8286eb5de868e2e65cb39fa42f19e4875acd61dd
RigorBytes <rigorbytes@gmail.com>
2026-05-16 00:49:17 +0300

    Update MicroPython code for sensors and wifi

--- secrets.py
+++ secrets.py
@@ -1,12 +1,12 @@
 # secrets.py
 WIFI_SSID = "ECE_Patras" # Το SSID του WiFi σου
-WIFI_PASS = "iloven'tniggers"
+WIFI_PASS = "12345678"
 
 ADMIN_PASS = "admin123" # Κωδικός για τη διαγραφή της μνήμης
 
 # Ρυθμίσεις για αποστολή Email (Απαιτείται App Password αν χρησιμοποιείς Gmail)
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 465
-SENDER_EMAIL = "to_email_sou@gmail.com"
-SENDER_PASSWORD = "to_app_password_sou"
-RECIPIENT_EMAIL = "leibadiotakesangelos@gmail.com"
+SENDER_EMAIL = "jimeandreadakis@gmail.com"
+SENDER_PASSWORD = "nvyuwpykguufkrxr"
+RECIPIENT_EMAIL = "jimeandreadakis@gmail.com"
